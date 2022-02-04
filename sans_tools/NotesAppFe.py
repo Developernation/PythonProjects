0d1fb354bed07fc725eebcf10e13cd8250541bd3
developernation <simeon.f.anderson@gmail.com>
2022-02-03 23:42:52 -0500

    added second tab

--- sans_tools/NotesAppFe.py
+++ sans_tools/NotesAppFe.py
@@ -1,5 +1,6 @@
 from doctest import master
 import tkinter as tk
+from tkinter import ttk
 from turtle import color, left
 from NotesApp import SansNotesApp as snp
 
@@ -18,15 +19,20 @@ min_width, min_height = 300,400
 
 label_text = ['Subject','Topic','Book','Page','Notes']
 
+
 window = tk.Tk()
+
+tabControl = ttk.Notebook(window)
 #window.geometry('800x1000+50+50')
 window.minsize(min_width, min_height)
 #window.maxsize(max_width, max_height)
 window.title('SANS NOTES APP')
 
+
+super_frame_tab1 = ttk.Frame(master=window,relief=border_effects['flat'])
 #first frame
 def build_frame(label_text_info,box_width):
-    frame1 = tk.Frame(master=window,relief=border_effects['flat'],width=100, height=10)
+    frame1 = tk.Frame(master=super_frame_tab1,relief=border_effects['flat'],width=100, height=10)
     text_box1 = tk.Entry(master=frame1, width=box_width, borderwidth=4)
     label1 = tk.Label(master=frame1, text=label_text_info,width=10)
     label1.pack(side='left')
@@ -50,7 +56,7 @@ frm3 = build_frame(label_text[3],5)
 
 
 
-frame3 = tk.Frame(master=window,relief=border_effects['flat'],width=50, height=10)
+frame3 = tk.Frame(master=super_frame_tab1,relief=border_effects['flat'],width=50, height=10)
 inputtxt = tk.Text(master= frame3, height = 5, width = 52,borderwidth=4,relief=border_effects['sunken'])
 label2 = tk.Label(master=frame3, text=label_text[4],width=10)
 label2.pack(side='left')
@@ -59,7 +65,7 @@ inputtxt.insert(tk.END,"test")
 frame3.pack(fill=tk.X)
 
 
-frame5 = tk.Frame(master=window,relief=border_effects['flat'],width=100, height=10)
+frame5 = tk.Frame(master=super_frame_tab1,relief=border_effects['flat'],width=100, height=10)
 label_opt = tk.Label(master=frame5, text='Options',width=10)
 
 Show_Button = tk.Button(master=frame5, 
@@ -85,7 +91,7 @@ Add_Button = tk.Button(master=frame5,
 Add_Button.pack(side='left')
 frame5.pack(fill=tk.X)
 
-frame4 = tk.Frame(master=window,relief=border_effects['flat'],width=100, height=10)
+frame4 = tk.Frame(master=super_frame_tab1,relief=border_effects['flat'],width=100, height=10)
 labeltxt = tk.Label(master=frame4, text='Study Data',width=10)
 Output = tk.Text(frame4, height = 50,
               width = 53,
@@ -93,6 +99,20 @@ Output = tk.Text(frame4, height = 50,
 labeltxt.pack(side='left')
 Output.pack(side='left')
 frame4.pack(fill=tk.X)
+super_frame_tab1.pack(fill=tk.X)
+tabControl.add(super_frame_tab1,text='Tab 1')
+#tabControl.pack(expand=1, fill="both",side='left')
 
+#### adding second tab
+super_frame_tab2 = ttk.Frame(master=window,relief=border_effects['flat'])
+super_frame_tab2.pack(fill=tk.X)
+tabControl.add(super_frame_tab2,text='Tab 2')
+
+
+
+
+tabControl.pack(expand=1, fill="both",side='right')
+
+#####
 
 window.mainloop()
