84d996eb6abbd57dbe4d67e970bbf703b125ba8d
developernation <simeon.f.anderson@gmail.com>
2022-02-16 22:50:32 -0500

    added getter setter and col normalization

--- sans_tools/app_utils.py
+++ sans_tools/app_utils.py
@@ -1,3 +1,4 @@
+from numpy import void
 from NotesApp import SansNotesApp
 import pandas as pd
 import os 
@@ -6,6 +7,22 @@ import os
 class AppFileHandler(SansNotesApp):
     def __init__(self):
         super().__init__()
+
+    @property
+    def set_ingest_file(self) -> str:
+        #getter
+        return self.ingest_file
+        
+    @set_ingest_file.setter
+    def set_ingest_file(self,file_name: str) -> void:
+        #setter
+        self.ingest_file = file_name
+    
+    def noralize_columns(self,**kwargs):
+        column_mappings = kwargs
+        
+
+        
         
 
 if __name__ == '__main__':
